a3fe52f30d9c85885fa268adfed3530c9ace50f7
Ramiro Boza <ramiro.boza@hotmail.com>
2023-03-16 15:24:02 -0300

    added scafolding for clients (#3)

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,58 @@
+class ClientsController < ApplicationController
+  before_action :set_client, only: %i[ show edit update destroy ]
+
+  # GET /clients or /clients.json
+  def index
+    @clients = Client.all
+  end
+
+  # GET /clients/1 or /clients/1.json
+  def show
+  end
+
+  # GET /clients/new
+  def new
+    @client = Client.new
+  end
+
+  # GET /clients/1/edit
+  def edit
+  end
+
+  # POST /clients or /clients.json
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to clients_path, notice: "Client was successfully created." 
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  # PATCH/PUT /clients/1 or /clients/1.json
+  def update
+    if @client.update(client_params)
+      redirect_to clients_path, notice: "Client was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  # DELETE /clients/1 or /clients/1.json
+  def destroy
+    @client.destroy
+    redirect_to clients_path, notice: "Client was successfully destroyed." 
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_client
+      @client = Client.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def client_params
+      params.require(:client).permit(:name, :email, :location)
+    end
+end
---
+++ app/models/client.rb
@@ -0,0 +1,4 @@
+class Client < ApplicationRecord
+    validates :name,:email,:location , presence:true 
+    validates :name, :email , uniqueness:true
+end
