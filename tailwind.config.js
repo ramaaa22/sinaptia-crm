073ff279f98cb9e6ea464e0393a2cd1d639f306a
Ramiro Boza <ramiro.boza@hotmail.com>
2023-03-21 15:52:42 -0300

    Styles/authentication forms (#9)
    * first changes
    
    * refactor
    
    * finished login and sign up
    
    * indent
    
    * fix indent
    
    * changed border on focus

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+    './node_modules/flowbite/**/*.js'
+  ],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  theme: {
+    extend: {
+      colors: {
+        default: '#2e3746',
+        sinaptia: {
+          1: '#19FFE4',
+          2: '#00B7A1',
+          3: '#009E8C',
+          4: '#EAB308',
+          5: '#FACC15'
+        }
+      }
+    }
+  }
 }
