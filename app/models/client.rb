f8c967c58066e4327387b80e01a9cf0e26aa2c0e
Ramiro Boza <ramiro.boza@hotmail.com>
2023-03-28 12:27:02 -0300

    Added filter tag for clients (#25)
    * added tags filters for clients
    
    * removed comment

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,6 @@
 class Client < ApplicationRecord
   include PgSearch::Model
-  pg_search_scope :search, against: [:name, :email, :location, :tags], using: {tsearch: {prefix: true}}
+  pg_search_scope :search, against: [:name, :email, :location], associated_against: {tags: [:name]}, using: {tsearch: {prefix: true}}
 
   has_many :notes, dependent: :delete_all
   has_and_belongs_to_many :tags
