6fabd9b24003a5fb60dca7281431a8f9dad6e2bd
AFLP2199 <fernandopiceda99@gmail.com>
2023-03-29 17:07:59 -0300

    change company_id to company

--- db/migrate/20230329200129_add_company_to_clients.rb
+++ db/migrate/20230329200129_add_company_to_clients.rb
@@ -1,5 +1,5 @@
 class AddCompanyToClients < ActiveRecord::Migration[7.0]
   def change
-    add_reference :clients, :company_id, null: true, foreign_key: true
+    add_reference :clients, :company, null: true, foreign_key: true
   end
 end
--- db/migrate/20230329200143_add_company_to_tags.rb
+++ db/migrate/20230329200143_add_company_to_tags.rb
@@ -1,5 +1,5 @@
 class AddCompanyToTags < ActiveRecord::Migration[7.0]
   def change
-    add_reference :tags, :company_id, null: true, foreign_key: true
+    add_reference :tags, :company, null: true, foreign_key: true
   end
 end
