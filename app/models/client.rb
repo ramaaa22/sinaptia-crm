e31fefc9365452903d2facf61e217d3c8741b82a
Ramiro Boza <ramiro.boza@hotmail.com>
2023-03-30 14:41:23 -0300

    Users index (#31)
    * table for users
    
    * added role to users
    
    * added restrictions
    
    * only allow edit or delete specific notes
    
    * changes in notes index
    
    * removed unnecesary initialize
    
    * replaced condition using admin?
    
    * organize controllers and schema
    
    * organize controllers and db schema
    
    * changes
    
    * removed edit in controllers
    
    * show tags only if admin
    
    * fix for bad refactor about edit
    
    * reorder application_controller
    
    * changed default user role to 0
    
    * reorganize db schema
    
    * fix db schema
    
    * refactor code
    
    * add destroy and edit in users controller
    
    * edit user form styles
    
    * creation of users by admin
    
    * add back link in edit
    
    * changes to create form for user
    
    * add error messages to user forms
    
    * added validations for first and last name
    
    * Users/policies (#27)
    
    * added policy to user
    
    * solve conflict
    
    * more validations
    
    * Users/styles (#28)
    
    * include search bar in users index
    
    * form error styles
    
    * change colors for error in light mode
    
    * hide button to edit or delete admin
    
    * add name to label
    
    * reuse form inputs for users route
    
    * refactor for errors
    
    * added spaces in forms
    
    * removed unnecesary line in users controller
    
    * refactor user policies
    
    * added field
    
    * removed unnecesary lines
    
    * add form file with conditional
    
    * fixes in routes
    
    * removed delete all for dependent models
    
    * remove dependency to notes from user model
    
    ---------
    
    Co-authored-by: AFLP2199 <fernandopiceda99@gmail.com>
    Co-authored-by: Agustin Lopez <59517612+AFLP2199@users.noreply.github.com>

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,7 @@ class Client < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search, against: [:name, :email, :location], associated_against: {tags: [:name]}, using: {tsearch: {prefix: true}}
 
-  has_many :notes, dependent: :delete_all
+  has_many :notes
   has_and_belongs_to_many :tags
 
   validates :name, :email, :location, presence: true
